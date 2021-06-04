59233bbc01b65267ffbc7db8a1f9d8b70fef3a89
Tyler Whidden <Ty.whidden@gmail.com>
2021-06-04 16:09:23 -0400

    Changing redux application to use reduxjstoolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
-import redux, { createStore} from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
+
 
 const initialState = { counter: 0, showCounter: true }
 
@@ -6,40 +7,74 @@ const initialState = { counter: 0, showCounter: true }
 // this can be exported 
 export const INCREMENT = 'increment';
 
-const counterReducer = (state = initialState, action) => {
-    if (action.type === INCREMENT) {
-        state.counter++;
-        
-        return {
-            counter: state.counter,
-            showCounter: state.showCounter
-        }
-    }
 
-    if (action.type === 'increase') {
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter,
-        };
+// This is demonstrating how to use reduxjstoolkit to make using redux easier
+createSlice({
+    name: 'counter',
+    initialState: initialState,
+    reducers: {
+        // can add our methods here
+        increment(state) {
+         // here with REDUXJSTOOLKIT only
+         // we can change the state directly
+         // Should NOT do this in regular redux
+         state.counter++;   
+         // this is allowed because of a package reduxtoolkit contains
+         // it basically checks for code like this where the state may be affected directly
+         // and deals with it for us
+        },
+        decreement(state) {
+            state.counter--;
+        },
+        // we didn't use action in the others so we didn't pass as a parameter
+        increase(state, action) {
+            state.counter = state.counter + action.amount;
+        },
+        toggle(state) {
+            state.showCounter = !state.showCounter;
+        },
     }
+});
 
-    if (action.type === 'decrement') {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter,
-        };
-    }
+const store = configureStore({
     
-    if (action.type === 'toggle') {
-        return {
-            counter: state.counter,
-            showCounter: !state.showCounter,
-        };
-    }
+});
 
-    return state;
-}
+// import redux, { createStore} from 'redux';
+// const counterReducer = (state = initialState, action) => {
+//     if (action.type === INCREMENT) {
+//         state.counter++;
+        
+//         return {
+//             counter: state.counter,
+//             showCounter: state.showCounter
+//         }
+//     }
 
-const store = createStore(counterReducer);
+//     if (action.type === 'increase') {
+//         return {
+//             counter: state.counter + action.amount,
+//             showCounter: state.showCounter,
+//         };
+//     }
+
+//     if (action.type === 'decrement') {
+//         return {
+//             counter: state.counter - 1,
+//             showCounter: state.showCounter,
+//         };
+//     }
+    
+//     if (action.type === 'toggle') {
+//         return {
+//             counter: state.counter,
+//             showCounter: !state.showCounter,
+//         };
+//     }
+
+//     return state;
+// }
+
+// const store = createStore(counterReducer);
 
 export default store;
